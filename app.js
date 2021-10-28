ec0d64d0c6f25f98c806de8002c5d302cefafa46
kamhar-ngado <kamhar.ngado.id@gmail.com>
2021-10-29 01:30:37 +0900

    waktunya istrahat

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var h1 = document.querySelector("h1");
 h1.className = "cool";
 
 let ans1 = document.getElementById("B1");
+let submit = document.getElementById("submit");
+
 ans1.addEventListener("click", function () {
   console.log("click click");
+});
+
+submit.addEventListener("click", function () {
+  if (ans1.value){
+    console.log("success")
+  }
 });
