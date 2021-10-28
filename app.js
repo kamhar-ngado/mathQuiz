5bac4b8501cdbf1b3ad3dbaf07f64b1b1f03d289
kamhar ngado <52423991+kamhar-ngado@users.noreply.github.com>
2021-10-29 00:21:56 +0900

    add my projects to github

---
+++ app.js
@@ -0,0 +1,7 @@
+var h1 = document.querySelector("h1");
+h1.className = "cool";
+
+let ans1 = document.getElementById("B1");
+ans1.addEventListener("click", function () {
+  console.log("click click");
+});
