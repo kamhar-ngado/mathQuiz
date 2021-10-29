840cd8afe3232d3b5897fdac1237595dc1a9df3f
kamhar-ngado <kamhar.ngado.id@gmail.com>
2021-10-29 09:02:05 +0900

    delete old codes

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-// let h1 = document.querySelector("h1");
-// h1.className = "cool";
 
-// let ans1 = document.getElementById("B1");
-// let submit = document.getElementById("submit");
-
-// ans1.addEventListener("click", function () {
-//   console.log("click click");
-// });
-
-// submit.addEventListener("click", function () {
-//   if (ans1.value){
-//     console.log("success")
-//   }
-// });
 
 var radios = document.getElementsByName("MC1");
 var val = "";
