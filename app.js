22d942183860112de7237124fbcb066f7df1822d
kamhar-ngado <kamhar.ngado.id@gmail.com>
2021-10-29 09:24:51 +0900

    fix answers not detect

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-
-
-var radios = document.getElementsByName("MC1");
-var val = "";
 var submit = function () {
+  var radios = document.getElementsByName("MC1");
+  var val = "";
   for (var i = 0, length = radios.length; i < length; i++) {
     if (radios[i].checked) {
       val = radios[i].value;
       break;
     }
   }
-};
 
-if (val == "") {
-  alert("opsi jawaban tidak boleh kosong");
-} else if (val == "uncorrect") {
-  alert("jawabanmu salah");
-} else {
-  alert("hore!! jawabanmu benar");
-}
+  if (val == "") {
+    alert("please select choice answer");
+  } else if (val == "correct") {
+    alert("Answer is correct !");
+  } else {
+    alert("Answer is wrong");
+  }
+};
